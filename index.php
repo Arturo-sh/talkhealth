443fb035a31854a33c045031fe1883465912a6c3
Arturo -sh <salashernandezarturo1512@gmail.com>
2023-09-27 14:53:05 -0600

    Cambios con xs

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="Aplicación de telemédicina orientada al público en general de toda la república mexicana">
 
     <!-- favicon -->
-    <link rel="shortcut icon" href="assets/img/favicon.png" />
+    <link rel="shortcut icon" href="assets/img/logo_app.jpg" />
 
     <!-- Bootstrap 3.3.2 -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
@@ -23,58 +23,16 @@
     <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
 
   </head>
-  <body class="login-page bg-login">
-    <div class="login-box">
-      <div class="login-logo bg-azul">
-        <img style="margin-top:-12px" src="assets/img/logo-blue.png" alt="Logo" height="50"> <b>Talkhealth</b>
-      </div><!-- /.login-logo -->
-      <?php  
- 
-      if (empty($_GET['alert'])) {
-        echo "";
-      } 
-
-      elseif ($_GET['alert'] == 1) {
-        echo "<div class='alert alert-danger alert-dismissable'>
-                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-                <h4>  <i class='icon fa fa-times-circle'></i> Error de acceso!</h4>
-               Usuario y/o contraseña incorrectos
-              </div>";
-      }
-
-      elseif ($_GET['alert'] == 2) {
-        echo "<div class='alert alert-success alert-dismissable'>
-                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-                <h4>  <i class='icon fa fa-check-circle'></i> Exito!!</h4>
-              Has salido con éxito.
-              </div>";
-      }
-      ?>
-
-      <div class="login-box-body">
-        <p class="login-box-msg"><i class="fa fa-user icon-title"></i>INICIO DE SESIÓN</p>
-        <br/>
-        <form action="login-check.php" method="POST">
-          <div class="form-group has-feedback">
-            <input type="text" class="form-control" name="username" placeholder="Usuario" autocomplete="off" required />
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-          </div>
-
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="Contraseña" required />
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-          <br/>
-          <div class="row">
-            <div class="col-xs-12">
-              <input type="submit" class="btn btn-info btn-lg btn-block btn-flat" name="login" value="Continuar" />
-            </div><!-- /.col -->
-          </div>
-        </form>
-
-      </div><!-- /.login-box-body -->
-    </div><!-- /.login-box -->
-
+  <body>
+    <div class="container">
+  <div class="container-fluid">
+  <div class="row">
+  <div class="col-xs-12 col-sm-6 col-md-4"><img src="assets/img/logo_app.jpg"></div>
+  <div class="col-xs-12 col-sm-6 col-md-4">.col-xs-6 .col-md-4</div>
+  </div>
+</div>
+</div>
+</div>
     <!-- jQuery 2.1.3 -->
     <script src="assets/plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
